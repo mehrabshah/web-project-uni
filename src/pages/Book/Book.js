bb4a7c8fd1fdf2d932438f673b001dae3909bf3e
Mehrab-developer <110405427+Mehrab-developer@users.noreply.github.com>
2023-10-16 15:09:37 +0500

    email fix

--- src/pages/Book/Book.js
+++ src/pages/Book/Book.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef } from 'react';
 import "./Book.css";
 import Hero from "./../../components/Hero/Hero";
 import { BiRightArrow } from "react-icons/bi";
+import emailjs from '@emailjs/browser';
+
 
 
 const formInputs = [
@@ -10,31 +12,58 @@ const formInputs = [
     type: 'text',
     id: 'name',
     placeholder: 'Write Your Name',
+    name:"from_name",
+  },
+  {
+    label: 'Email',
+    type: 'email',
+    id: 'email',
+    placeholder: 'Write Your email',
+    name:"from_email",
   },
   {
     label: 'Phone',
     type: 'tel',
     id: 'phone',
     placeholder: 'Write Your Phone',
+    name:"phone",
   },
   {
     label: 'Date',
     type: 'date',
     id: 'date',
     placeholder: 'Select a Date',
+    name:"date",
   },
   {
     label: 'Time',
     type: 'time',
     id: 'time',
     placeholder: 'Select a Time',
+    name:"time",
   },
 ];
 
 export default function Book() {
+
+  const form = useRef();
   const sendEmail = (e) => {
-    e.preventDefault(); // prevents the page from reloading when you hit “Send”
+    e.preventDefault();
+
+    
+
+    emailjs.sendForm('service_tvwe0yg', 'template_v670i6g', form.current, 'N75vtM6011NslOkEG')
+      .then((result) => {
+          window.alert("Email has been sent")
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
   };
+
+
+
+ 
 
   return (
     <>
@@ -58,7 +87,7 @@ export default function Book() {
 
           <div className="row mt-5">
             <div className="col-md-6 m-auto">
-            <form onSubmit={sendEmail}>
+            <form ref={form} onSubmit={sendEmail}>
       {formInputs.map((input) => (
         <div key={input.id} className="mb-3 d-flex align-items-center">
           <label htmlFor={input.id} className="form-label me-3">
@@ -69,12 +98,13 @@ export default function Book() {
             className="form-control"
             id={input.id}
             placeholder={input.placeholder}
+            name={input.name}
+            
           />
         </div>
       ))}
-      {/* <button type="submit" className="btn btn-primary">
-        Submit
-      </button> */}
+     
+      <input type="submit" value="Send" className='send-button' />
     </form>
 
 
